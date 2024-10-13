6d5a70a1b2d920d76f4892f33d163cd3532c2c67
SgtMilk <alix.routhier-lalonde@hotmail.com>
2024-10-13 22:01:49 +0000

    first iteration for PR

--- src/bird.cpp
+++ src/bird.cpp
@@ -1,35 +1,43 @@
 #include "bird.h"
 
+#include <iostream>
+
 const float Bird::BIRD_SCALE = 0.2;
 const int Bird::COLLISION_TOLERANCE = 25;
 const int Bird::NUM_COLLISIONS_TO_TRIGGER_PER_SECOND = 300;
 
-Bird::Bird(sf::Vector2f center, int frameRate)
+Bird::Bird(sf::Vector2f InCenter, int InFrameRate)
 {
+    frameRate = InFrameRate;
+
     bool res = birdTexture.loadFromFile("./lib/bird.png");
-    if(!res) throw("Cannot load bird\n");
+    if(!res) {
+        std::cout << "Cannot load bird image. Exiting.\n";
+        isError = true;
+        return;
+    };
+
     bird.setTexture(birdTexture, true);
     bird.setScale(BIRD_SCALE, BIRD_SCALE);
 
     sf::Vector2f dimensions = bird.getLocalBounds().getSize();
-    this->bird.setOrigin(dimensions.x / 2, dimensions.y / 2);
-    this->bird.setPosition(center);
-
-    speed = 0;
-    this->frameRate = frameRate;
+    bird.setOrigin(dimensions.x / 2, dimensions.y / 2);
+    reset(InCenter);
 
     acc = 9.8 / frameRate * 1.5;
     negAcc = acc * 2;
 
     collisionCounter = 0;
     NUM_COLLISIONS_TO_TRIGGER = NUM_COLLISIONS_TO_TRIGGER_PER_SECOND / frameRate;
+
+    isError = false;
 }
 
 sf::Sprite Bird::updatePosition()
 {
     speed += (speed >= 0 ? negAcc : acc);
 
-    sf::Vector2f position = this->bird.getPosition();
+    sf::Vector2f position = bird.getPosition();
     position.y += speed;
     bird.setPosition(position);
 
@@ -41,7 +49,7 @@ void Bird::jump()
     speed = -7;
 }
 
-bool Bird::checkCollision(sf::Sprite *pipeSprites, int numPipes){
+bool Bird::checkCollision(std::vector<sf::Sprite> pipeSprites, int numPipes){
     sf::Vector2f birdPos = bird.getPosition();
     float width = bird.getGlobalBounds().width;
     float height = bird.getGlobalBounds().height;
@@ -82,4 +90,15 @@ bool Bird::isOOO(int screenHeight){
     float limit = screenHeight / 4;
 
     return y < -1 * limit || y > limit + screenHeight;
+}
+
+void Bird::reset(sf::Vector2f center)
+{
+    bird.setPosition(center);
+    speed = 0;
+}
+
+bool Bird::getIsError()
+{
+    return isError;
 }
